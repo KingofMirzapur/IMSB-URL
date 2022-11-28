cb808f36f79910dd2992bcc6fee49e230028d3d3
iFilms Botz <ifilmsbotz@gmail.com>
2022-11-29 00:56:01 +0530

    Update and rename app.json to sample.py

---
+++ sample.py
@@ -0,0 +1,5 @@
+ADMINS = 5858245241
+AUTH_CHANNEL = -1001668257871
+CHANNELS = -1001512591839 -1001740810830 -1001553525090
+PICS = https://graph.org/file/1571cf70ff4f00c71c45f.jpg
+UPSTREAM_REPO = https://iflilmsbotz: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@github.com/ifilmsbotz/imsb-url
