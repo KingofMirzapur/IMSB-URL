a1fdf8211dff0b1f455a744519557aff1c1892b4
iFilms Botz <ifilmsbotz@gmail.com>
2022-11-28 19:11:54 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,12 +3,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/iFilmsBotz/IMSB.git /IMSB
+  git clone https://github.com/iFilmsBotz/IMSB-URL.git /IMSB-URL
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /IMSB
+  git clone $UPSTREAM_REPO /IMSB-URL
 fi
-cd /IMSB
+cd /IMSB-URL
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
